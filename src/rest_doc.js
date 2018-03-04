548d206b2468d42fbaa435c3321b086b160cb6e8
Lambert Lum <emceelam@gmail.com>
2018-03-03 17:24:30 -0800

    adjust the button address id if we delete the address.

--- src/rest_doc.js
+++ src/rest_doc.js
@@ -3,21 +3,18 @@ import AjaxAddress from "./AjaxAddress";
 
 function rest_doc_start () {
   var ajax_address = new AjaxAddress();
-  
   var id_to_address = {};
+
   ajax_address.promise_get_all().then(function(addresses) {
     addresses.forEach( function (address) {
       let id = address['id'];
       id_to_address[id] = address;
-
     });
-    let ids = Object.keys(id_to_address);
-    let first_id = ids[0];
-    $("#get_address_id").text(first_id);
-    $("#put_address_id").text(first_id);
-    $("#delete_address_id").text(first_id);
+
+    set_button_address_id(id_to_address);
     $("#get_address_id_button").click(function () {
-      ajax_address.promise_get_one(first_id).then(function(address) {
+      let id = $("#get_address_id").text();
+      ajax_address.promise_get_one(id).then(function(address) {
         $("#get_address_id_output").text(JSON.stringify(address, null, 4));
       });
     });
@@ -41,20 +38,19 @@ function rest_doc_start () {
   $("#post_address_body").text(JSON.stringify(post_data, null, 4));
   $("#post_address_button").click(function() {
     ajax_address.promise_post(post_data).then( function(address) {
+      let id = address["id"];
+      id_to_address[id] = address;
+      set_button_address_id (id_to_address);
       $("#post_address_output").text(JSON.stringify(address, null, 4));
     });
   });
 
   $("#delete_address_button").click(function() {
     var delete_id = $("#delete_address_id").text();
-    ajax_address.promise_delete(delete_id).then( function () {
+    ajax_address.promise_delete(delete_id).then( function (data) {
       console.log (`delete ${delete_id}`);
       delete id_to_address[delete_id];
-      let ids = Object.keys (id_to_address);
-      let first_id = ids[0];
-      $("#get_address_id").text(first_id);
-      $("#put_address_id").text(first_id);
-      $("#delete_address_id").text(first_id);
+      set_button_address_id(id_to_address);
     });
   });
 
@@ -80,4 +76,21 @@ function rest_doc_start () {
 $(document).ready(function() {
   rest_doc_start();
 });
+
+function set_button_address_id(id_to_address) {
+  let ids = Object.keys(id_to_address);
+  let id = ids.length ? ids[0] : "{id}";
+  $("#get_address_id").text(id);
+  $("#put_address_id").text(id);
+  $("#delete_address_id").text(id);
+
+  if (ids.length) {
+    return;
+  }
+
+  $("#get_address_id_button").prop("disabled", true);
+  $("#put_address_button").prop("disabled", true);
+  $("#delete_address_button").prop("disabled", true);
+}
+
 
