c36acb72c4e6ad69df171fe65fb3c9de9c50f909
Lambert Lum <emceelam@gmail.com>
2018-05-25 02:20:11 -0700

    Use more of the done handler within jQuery.ajax(). Add a http status result

--- src/AjaxAddress.js
+++ src/AjaxAddress.js
@@ -6,42 +6,38 @@ export default class AjaxAddresses {
     this.output_error = output_error;   // output_error("error text")
   }
 
-  promise_get_all () {
-    return new Promise( (resolve) => {
-      $.ajax({
+  done_get_all ( done_handler ) {
+    $.ajax({
         type: "GET",
         url: `http://${Conf.host}:${Conf.port}/api/addresses`,
-        success: resolve,
         error: this.reject.bind(this),
-      });
-    });
+    })
+      .done ( done_handler );
   }
-  promise_get_one (id) {
-    return new Promise( (resolve) => {
-      $.ajax({
+
+  done_get_one(id, done_handler) {
+    $.ajax({
         type: "GET",
         url: `http://${Conf.host}:${Conf.port}/api/addresses/${id}`,
-        success: resolve,
         error: this.reject.bind(this),
-      });
-    });
+    })
+      .done ( done_handler );
   }
-  promise_delete (id) {
-    return new Promise ( (resolve) => {
-      $.ajax({
+
+  done_delete (id, done_handler) {
+    $.ajax({
         type: "DELETE",
         url: `http://${Conf.host}:${Conf.port}/api/addresses/${id}`,
         xhrFields: {
           withCredentials: true
         },
-        success: resolve,
         error: this.reject.bind(this),
-      });
-    });
+    })
+      .done( done_handler );
   }
-  promise_post (addr_data) {
-    return new Promise ( (resolve) => {
-      $.ajax({
+
+  done_post (addr_data, done_handler) {
+    $.ajax({
         type: "POST",
         url: `http://${Conf.host}:${Conf.port}/api/addresses`,
         xhrFields: {
@@ -52,16 +48,15 @@ export default class AjaxAddresses {
           "Content-Type" : "application/json",
         },
         dataType: "json",
-        success: resolve,
         error: this.reject.bind(this),
-      });
-    });
+    })
+      .done( done_handler );
   }
-  promise_put (address) {
-    var id = address["id"];
+
+  done_put (address, done_handler) {
+    let id = address["id"];
     delete address["id"];
-    return new Promise ( (resolve) => {
-      $.ajax({
+    $.ajax({
         type: "PUT",
         url: `http://${Conf.host}:${Conf.port}/api/addresses/${id}`,
         xhrFields: {
@@ -72,10 +67,9 @@ export default class AjaxAddresses {
           "Content-Type" : "application/json",
         },
         dataType: "json",
-        success: resolve,
         error: this.reject.bind(this),
-      });
-    });
+    })
+      .done( done_handler );
   }
 
   promise_geocode (address) {
--- src/rest_doc.js
+++ src/rest_doc.js
@@ -7,28 +7,31 @@ function rest_doc_start () {
   );
   var id_to_address = {};
 
-  ajax_address.promise_get_all().then(function(addresses) {
+  ajax_address.done_get_all (function (addresses, parm2, xhr) {
     addresses.forEach( function (address) {
       let id = address['id'];
       id_to_address[id] = address;
     });
 
+    console.log(id_to_address);
     set_button_address_id(id_to_address);
     $("#get_address_id_button").click(function () {
       let id = $("#get_address_id").text();
-      ajax_address.promise_get_one(id).then(function(address) {
-        $("#get_address_id_output").text(JSON.stringify(address, null, 4));
+      ajax_address.done_get_one(id, function(address, parm2, xhr) {
+        $("#card_get_address .result_body").text(JSON.stringify(address, null, 4));
+        let status_line = `${xhr.status} ${xhr.statusText}`;
+        $("#card_get_address>div.card-header").text(status_line);
       });
     });
   });
 
   console.log ("rest_doc_start()");
   $("#get_addresses_button").click(function() {
-    ajax_address.promise_get_all().then(
-      function(addresses) {
-        $("#get_addresses_output").text(JSON.stringify(addresses, null, 4));
-      }
-    );
+    ajax_address.done_get_all(function (addresses, parm2, xhr) {
+      $("#card_get_addresses .result_body").text(JSON.stringify(addresses, null, 4));
+      let status_line = `${xhr.status} ${xhr.statusText}`;
+      $("#card_get_addresses>div.card-header").text(status_line);
+    });
   });
   
   var post_data = {   // Weird Stuff Warehouse
@@ -39,20 +42,25 @@ function rest_doc_start () {
   };
   $("#post_address_body").text(JSON.stringify(post_data, null, 4));
   $("#post_address_button").click(function() {
-    ajax_address.promise_post(post_data).then( function(address) {
+    ajax_address.done_post(post_data, function(address, parm2, xhr) {
       let id = address["id"];
       id_to_address[id] = address;
       set_button_address_id (id_to_address);
-      $("#post_address_output").text(JSON.stringify(address, null, 4));
+      $("#card_post_address .result_body").text(JSON.stringify(address, null, 4));
+      let status_line = `${xhr.status} ${xhr.statusText}`;
+      $("#card_post_address>.card-header").text(status_line);
     });
   });
 
   $("#delete_address_button").click(function() {
     var delete_id = $("#delete_address_id").text();
-    ajax_address.promise_delete(delete_id).then( function (data) {
-      console.log (`delete ${delete_id}`);
+    ajax_address.done_delete(delete_id, function(parm1,parm2,xhr) {
+      let address = id_to_address[delete_id];
       delete id_to_address[delete_id];
       set_button_address_id(id_to_address);
+      //$("#card_delete_address .result_body").text(JSON.stringify(address, null, 4));
+      let status_line = `${xhr.status} ${xhr.statusText}`;
+      $("#card_delete_address>.card-header").text(status_line);
     });
   });
 
@@ -66,10 +74,10 @@ function rest_doc_start () {
   $("#put_address_button").click(function() {
     put_data["id"] = $("#put_address_id").text();
     console.log("put: " + put_data["id"]);
-    ajax_address.promise_put(put_data).then( function (address) {
-      //console.log("promise_put resolves");
-      //console.log(address);
-      $("#put_address_output").text(JSON.stringify(address, null, 4));
+    ajax_address.done_put(put_data, function(address, parm2, xhr) {
+      $("#card_put_address .result_body").text(JSON.stringify(address, null, 4));
+      let status_line = `${xhr.status} ${xhr.statusText}`;
+      $("#card_put_address>.card-header").text(status_line);
     });
   });
 }
