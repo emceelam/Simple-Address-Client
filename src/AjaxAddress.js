b254d020afe22840dc013dffc88a256808ae9570
Lambert Lum <emceelam@gmail.com>
2018-05-25 02:33:01 -0700

    put back in the promise functions. Some code still depends on it

--- src/AjaxAddress.js
+++ src/AjaxAddress.js
@@ -72,6 +72,78 @@ export default class AjaxAddresses {
       .done( done_handler );
   }
 
+  promise_get_all () {
+    return new Promise( (resolve) => {
+      $.ajax({
+        type: "GET",
+        url: `http://${Conf.host}:${Conf.port}/api/addresses`,
+        success: resolve,
+        error: this.reject.bind(this),
+      });
+    });
+  }
+  promise_get_one (id) {
+    return new Promise( (resolve) => {
+      $.ajax({
+        type: "GET",
+        url: `http://${Conf.host}:${Conf.port}/api/addresses/${id}`,
+        success: resolve,
+        error: this.reject.bind(this),
+      });
+    });
+  }
+  promise_delete (id) {
+    return new Promise ( (resolve) => {
+      $.ajax({
+        type: "DELETE",
+        url: `http://${Conf.host}:${Conf.port}/api/addresses/${id}`,
+        xhrFields: {
+          withCredentials: true
+        },
+        success: resolve,
+        error: this.reject.bind(this),
+      });
+    });
+  }
+  promise_post (addr_data) {
+    return new Promise ( (resolve) => {
+      $.ajax({
+        type: "POST",
+        url: `http://${Conf.host}:${Conf.port}/api/addresses`,
+        xhrFields: {
+          withCredentials: true
+        },
+        data : JSON.stringify(addr_data),
+        headers: {
+          "Content-Type" : "application/json",
+        },
+        dataType: "json",
+        success: resolve,
+        error: this.reject.bind(this),
+      });
+    });
+  }
+  promise_put (address) {
+    var id = address["id"];
+    delete address["id"];
+    return new Promise ( (resolve) => {
+      $.ajax({
+        type: "PUT",
+        url: `http://${Conf.host}:${Conf.port}/api/addresses/${id}`,
+        xhrFields: {
+          withCredentials: true
+        },
+        data: JSON.stringify(address),
+        headers: {
+          "Content-Type" : "application/json",
+        },
+        dataType: "json",
+        success: resolve,
+        error: this.reject.bind(this),
+      });
+    });
+  }
+
   promise_geocode (address) {
     var street = encodeURI(address["street"]);
     var city   = encodeURI(address["city"]);
